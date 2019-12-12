d0b1b245d804d1e34046b0fdf1c8e1b855fa925c
AleksMat <matej.aleksandrov@sinergise.com>
2019-12-12 20:15:53 +0100

    added strip parsing parameter

--- fast_aoc/parsing.py
+++ fast_aoc/parsing.py
@@ -3,13 +3,15 @@
 import collections
 
 
-def parse_input(typ=None, sep=' '):
+def parse_input(typ=None, sep=' ', strip=' '):
     """
     :param typ: Type of resulting input. By default it will try to parse every item in either int or float.
         If that won't work it will leave it in string form
     :type typ: type
     :param sep: A string separator between multiple entries in a single line
     :type sep: str
+    :param strip: Characters that will be stripped from each item in each line
+    :type strip: str
     """
     is_function = isinstance(typ, collections.Callable)
     real_input_type = None if is_function else typ
@@ -18,7 +20,7 @@ def parse_input(typ=None, sep=' '):
 
         def parsing_solution(raw_input):
 
-            parsed_input = [parse_line(line, typ=real_input_type, sep=sep)
+            parsed_input = [parse_line(line, typ=real_input_type, sep=sep, strip=strip)
                             for line in raw_input.strip('\n').split('\n')]
 
             if len(parsed_input) == 1:
@@ -34,10 +36,10 @@ def parse_input(typ=None, sep=' '):
     return parse_decorator
 
 
-def parse_line(line, typ=None, sep=' '):
+def parse_line(line, typ=None, sep=' ', strip=' '):
     """ Parse a single line
     """
-    parsed_line = [parse_item(item, typ=typ) for item in line.strip().strip(sep).split(sep)]
+    parsed_line = [parse_item(item, typ=typ, strip=strip) for item in line.strip(strip + sep).split(sep)]
 
     if len(parsed_line) == 1:
         return parsed_line[0]
@@ -45,11 +47,12 @@ def parse_line(line, typ=None, sep=' '):
     return parsed_line
 
 
-def parse_item(item, typ=None):
+def parse_item(item, typ=None, strip=' '):
     """ Parse a single item
     """
-    type_candidates = [int, float] if typ is None else [typ]
+    item = item.strip(strip)
 
+    type_candidates = [int, float] if typ is None else [typ]
     for type_candidate in type_candidates:
         try:
             return type_candidate(item)
